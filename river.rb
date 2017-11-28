93a73e9e99ffa2a13e378cdc615b3291a4fe69e8
Graeme Brown <graemehbrown@googlemail.com>
2017-11-28 22:45:25 +0000

    added fish_count method to River class.  Refactored tests to use the new count method.  Exercise completed.

--- river.rb
+++ river.rb
@@ -1,19 +1,23 @@
 class River
 
-attr_reader :name, :fish
+  attr_reader :name, :fish
 
-  def initialize(name, fish)
-    @name = name
-    @fish = fish
+    def initialize(name, fish)
+      @name = name
+      @fish = fish
+    end
+
+  def add_fish(fish)
+    @fish.push(fish)
   end
 
-def add_fish(fish)
-  @fish.push(fish)
-end
+  def remove_fish(fish)
+    @fish.delete(fish)
+  end
 
-def remove_fish(fish)
-  @fish.delete(fish)
-end
+  def fish_count()
+    return @fish.count()
+  end
 
 
 end
--- specs/river_spec.rb
+++ specs/river_spec.rb
@@ -6,42 +6,50 @@ require_relative('../fish')
 class TestRiver < MiniTest::Test
 
   def setup
-    @carp = Fish.new('Carp')
-    @trout = Fish.new('Trout')
-    @perch = Fish.new('Perch')
-    fish = [@carp, @trout]
-    @hudson = River.new('Hudson', fish)
+      @carp = Fish.new('Carp')
+      @trout = Fish.new('Trout')
+      @perch = Fish.new('Perch')
+      fish = [@carp, @trout]
+      @hudson = River.new('Hudson', fish)
   end
 
-def test_get_name()
-  expected = 'Hudson'
-  assert_equal(expected, @hudson.name())
-end
+  def test_get_name()
+    expected = 'Hudson'
+    assert_equal(expected, @hudson.name())
+  end
 
-def test_get_fish()
-  expected = [@carp, @trout]
-  assert_equal(expected, @hudson.fish())
-end
+  def test_get_fish()
+    expected = [@carp, @trout]
+    assert_equal(expected, @hudson.fish())
+  end
 
-def test_add_fish()
-  fish_to_add = @perch
-  @hudson.add_fish(fish_to_add)
-  assert_equal(true, @hudson.fish().include?(fish_to_add))
-end
+  def test_add_fish()
+    fish_to_add = @perch
+    @hudson.add_fish(fish_to_add)
+    assert_equal(true, @hudson.fish().include?(fish_to_add))
+  end
 
-def test_remove_fish__removes_fish()
-  fish_to_remove = @carp
-  @hudson.remove_fish(fish_to_remove)
-  assert_equal(false, @hudson.fish().include?(fish_to_remove))
-  assert_equal(true, @hudson.fish().include?(@trout))
-end
+  def test_remove_fish__removes_fish()
+    fish_to_remove = @carp
+    @hudson.remove_fish(fish_to_remove)
+    assert_equal(false, @hudson.fish().include?(fish_to_remove))
+    assert_equal(true, @hudson.fish().include?(@trout))
+  end
 
-def test_remove_fish__reduces_fish_count()
-  fish_count_before = @hudson.fish.count()
-  fish_to_remove = @carp
-  @hudson.remove_fish(fish_to_remove)
-  fish_count_after = @hudson.fish.count()
-  assert_equal(fish_count_before-1, fish_count_after)
-end
+  def test_remove_fish__reduces_fish_count()
+    fish_count_before = @hudson.fish_count()
+    fish_to_remove = @carp
+    @hudson.remove_fish(fish_to_remove)
+    fish_count_after = @hudson.fish_count()
+    assert_equal(fish_count_before-1, fish_count_after)
+  end
+
+  def test_fish_count__adding_fish()
+    fish_count_before = @hudson.fish_count()
+    fish_to_add = @perch
+    @hudson.add_fish(fish_to_add)
+    fish_count_after = @hudson.fish_count()
+    assert_equal(fish_count_before+1, fish_count_after)
+  end
 
 end
